9a5074f1fa65ceebf8c27067c345df7c34c47592
Ashwani Singh <ashwani.singh@squareboat.com>
2022-01-14 18:16:58 +0530

    view ticket screen added

---
+++ lib/app/data/repository/create_ticket_repository.dart
@@ -0,0 +1,14 @@
+import 'package:starter/base/base_reposiotry.dart';
+
+class TicketRepository extends BaseRepositry {
+  // Future<RepoResponse<TicketList>> fetchAllTicket(
+  //     Map<String, dynamic> data) async {
+  //   final response = await controller.get(
+  //     path: URLs.ticketUrl,
+  //     options: Options(headers: data),
+  //   );
+  //   return response is APIException
+  //       ? RepoResponse(error: response)
+  //       : RepoResponse(data: TicketResponse.fromJson(response).data);
+  // }
+}
